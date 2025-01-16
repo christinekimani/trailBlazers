2f3720fe04ff91aba1fe43cf435b9f1717e3ffa7
Tony-Otieno <nitonito598@gmail.com>
2025-01-16 15:08:21 +0300

    initialCommit

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    # Render an HTML template for the home page
+    return render(request, 'home/index.html')
