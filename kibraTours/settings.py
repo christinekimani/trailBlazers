6bb676ced252a269135546d4ff84962321ee4f85
Tony-Omondi <otienotony598@gmail.com>
2025-01-26 11:19:33 +0300

    initial commit

--- kibraTours/settings.py
+++ kibraTours/settings.py
@@ -143,7 +143,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 SOCIALACCOUNT_PROVIDERS = {}
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-ACCOUNT_AUTHENTICATION_METHOD = "email"
-ACCOUNT_EMAIL_REQUIRED = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nitonito598@gmail.com'
+EMAIL_HOST_PASSWORD = 'fswhmbyksqxmowtp'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
